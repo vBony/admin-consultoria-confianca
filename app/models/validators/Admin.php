c84300d88e6391f1366914c4ae2dee0cfc457db6
VInicius <vini_07-ferreira@hotmail.com>
2022-01-29 17:47:56 -0300

    implementando validadores

--- app/models/validators/Admin.php
+++ app/models/validators/Admin.php
@@ -4,8 +4,11 @@ class Admin {
     public $messages = [];
     private $emptyMessage = 'Campo obrigatório';
 
+
     public function validate($data){
         $this->name($data);
+        $this->lastName($data);
+        $this->email($data);
     }
 
     public function getMessages(){
@@ -22,5 +25,55 @@ class Admin {
         }else{
             $this->messages['name'] = $this->emptyMessage;
         }
+    }
+
+    public function lastName($data){
+        $lastName = $data['lastName'];
+
+        if(!empty($lastName)){
+            if(strlen($lastName) <= 2 || strlen($lastName) > 20){
+                $this->messages['lastName'] = 'Dado inválido';
+            }
+        }
+    }
+
+    public function email($data){
+        $email = $data['email'];
+
+        if(empty($email)){
+            $this->messages['email'] = $this->emptyMessage;
+        }else{
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $this->messages['email'] = "Email inválido"; 
+            }
+        }
+    }
+
+    public function password($data){
+        $pass = $data['password'];
+
+        if(!empty($pass)){
+            if(strlen($pass) < 3){
+                $this->messages['password'] = 'A senha deve conter no mínimo 3 caracteres';
+            }
+        }else{
+            $this->messages['password'] = $this->emptyMessage;
+        }
+    }
+
+    public function retypePassword($data){
+        $pass = $data['password'];
+        $rpass = $data['retypePassword'];
+
+        if(!empty($rpass)){
+            if($pass != $rpass){
+                $this->messages['password'] = 'As senhas não conferem';
+                $this->messages['retypePassword'] = 'As senhas não conferem';
+            }
+        }
+    }
+
+    public function passwordOnCreate($data){
+
     }
 }
